085710c1e12ed75936bbb2c6884537ddb7989d26
Rafael Martins de Castro <42079080+Far3llProject@users.noreply.github.com>
2020-08-08 12:59:53 -0300

    Todo list commit

---
+++ src/pages/TodoList.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page>
+    <h6>TODOLIST</h6>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/todolist', name: 'todo', component: () => import('pages/TodoList.vue') }
     ]
   },
 
